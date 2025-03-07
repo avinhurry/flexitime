d265c1e1fdbe3a93328fab67fa604f9ae0c9d5a7
Avin Hurry <avinhurry@icloud.com>
2025-03-07 16:28:34 +0000

    Set required_minutes and utilise in view

--- app/models/week_entry.rb
+++ app/models/week_entry.rb
@@ -1,3 +1,15 @@
 class WeekEntry < ApplicationRecord
   belongs_to :user
+
+  before_create :set_required_minutes
+
+  def set_required_minutes
+    previous_week = user.week_entries.where("beginning_of_week < ?", beginning_of_week).order(beginning_of_week: :desc).first
+
+    return if previous_week.nil?
+
+    previous_offset = previous_week&.offset_in_minutes.to_i
+
+    self.required_minutes = previous_offset.abs
+  end
 end
