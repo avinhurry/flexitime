017f5f87c0fbb6d87da45e50a8519255c7b42e8d
Avin Hurry <avinhurry@icloud.com>
2025-03-14 12:28:53 +0000

    Add rspec setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,12 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # that will avoid rails generators crashing because migrations haven't been run yet
 # return unless Rails.env.test?
 require 'rspec/rails'
+require 'pry'
 # Add additional requires below this line. Rails is not loaded until this point!
+
+
+# Pull in all the files in spec/support automatically.
+Dir['./spec/support/**/*.rb'].each { |file| require file }
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -37,6 +42,9 @@ RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
+  config.include FactoryBot::Syntax::Methods
+  config.include AuthHelpers, type: :request
+  config.include ActiveSupport::Testing::TimeHelpers
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
