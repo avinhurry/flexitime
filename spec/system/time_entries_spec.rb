1024e253189fc1230bb2fe5599c5c96959a84a66
Avin Hurry <50492247+avinhurry@users.noreply.github.com>
2026-01-09 11:41:27 +0000

    Update/fix specs

--- spec/system/time_entries_spec.rb
+++ spec/system/time_entries_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.feature "Time entries", type: :system do
   let(:user) { create(:user, email: "user@example.com") }
 
-  scenario "records a time entry with a lunch break" do
+  scenario "records a time entry with a break" do
     travel_to Time.zone.local(2025, 3, 7, 9, 0) do
       given_i_am_signed_in_as(user)
       when_i_record_a_time_entry
@@ -20,8 +20,8 @@ RSpec.feature "Time entries", type: :system do
 
     fill_in "Clock in", with: "2025-03-07T09:00"
     fill_in "Clock out", with: "2025-03-07T17:00"
-    fill_in "Lunch in (optional)", with: "12:00"
-    fill_in "Lunch out (optional)", with: "12:30"
+    fill_in "Break start", with: "12:00"
+    fill_in "Break end", with: "12:30"
 
     click_on "Save entry"
   end
